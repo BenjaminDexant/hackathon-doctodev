75c7f9a89f2f63b69240efd26d62665c1839ac91
BenjaminDexant <benjamin.dexant@gmail.com>
2020-06-24 23:52:27 +0200

    link tu page info

--- src/components/InfoCountry.js
+++ src/components/InfoCountry.js
@@ -1,10 +1,7 @@
 import React from "react";
 import { Card } from "react-bootstrap";
-import { Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
 
-
-const Results = (props) => {
+const InfoCountry = (props) => {
   return (
     <Card>
       <Card.Header>{props.name}</Card.Header>
@@ -14,12 +11,9 @@ const Results = (props) => {
           <div>Type de soin : {props.description}</div>
           <div>Prix : {props.price}</div>
         </Card.Text>
-        <Link to="/Infos">
-          <Button variant="primary">Plus d'info</Button>
-        </Link>
       </Card.Body>
     </Card>
   );
 };
 
-export default Results;
+export default InfoCountry;
--- src/components/ResultsOrigin.jsx
+++ src/components/ResultsOrigin.jsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Card } from "react-bootstrap";
 import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const ResultsOrigin = (props) => {
+  const [country, setCountry] = useState(null);
+  useEffect(() => {
+    function handleClick(props) {
+      setCountry(props);
+    }
+  }
+
   return (
     <Card>
       <Card.Header>Votre pays : {props.name}</Card.Header>
@@ -12,10 +20,12 @@ const ResultsOrigin = (props) => {
           <div>Type de soin : {props.description}</div>
           <div>Prix : {props.price}</div>
         </Card.Text>
-        <Button variant="primary">Plus d'info</Button>
+        <Link to="/Infos">
+          <Button variant="primary" onClick={handleClick}>Plus d'info</Button>
+        </Link>
       </Card.Body>
     </Card>
   );
-}
+};
 
 export default ResultsOrigin;
