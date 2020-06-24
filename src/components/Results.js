2c090517250bccf18267fcce6801ba5c669a0733
BenjaminDexant <benjamin.dexant@gmail.com>
2020-06-24 15:14:46 +0200

    just started

---
+++ src/components/Results.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Result(props) {
+  return (
+    <Card>
+      <Card.Header>Pays</Card.Header>
+      <Card.Body>
+        <Card.Title>Special title treatment</Card.Title>
+        <Card.Text>
+          With supporting text below as a natural lead-in to additional content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default Result;
