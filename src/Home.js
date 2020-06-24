7201446771c6d5f00d39fa33d825cb08b33977f7
waledak <tanguy.cirillo@gmail.com>
2020-06-24 17:20:54 +0200

    create a form

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import FormResearch from './components/FormResearch'
 
 const Home = () => {
     return (
         <div>
             <h1>Page Home</h1>
             <Link to="Infos">Lien Page info</Link>
+            <FormResearch />
         </div>
     )
 }
